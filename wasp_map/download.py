aea35d2cca8589bea9bc6766b401caf46f9e68de
anthony-aylward <aaylward@eng.ucsd.edu>
2019-07-05 20:21:20 -0700

    test

--- wasp_map/download.py
+++ wasp_map/download.py
@@ -29,18 +29,18 @@ ANACONDA_HASH = os.environ.get(
     'WASP_MAP_ANACONDA_HASH',
     '45c851b7497cc14d5ca060064394569f724b67d9b5f98a926ed49b834a6bb73a'
 )
+CONDA_PATH = os.path.join(ANACONDA_DIR, 'bin', 'conda')
 
 
 
 
 # Functions ====================================================================
 
-def download_anaconda_install_script(anaconda_install_script_path, quiet=False):
-    if not quiet:
-        print(
-            'Downloading Anaconda3 install script to '
-            f'{anaconda_install_script_path}'
-        )
+def download_anaconda_install_script(anaconda_install_script_path):
+    print(
+        'Downloading Anaconda3 install script to '
+        f'{anaconda_install_script_path}'
+    )
     with urlopen(ANACONDA_URL) as (
         response
     ), open(anaconda_install_script_path, 'wb') as (
@@ -49,21 +49,30 @@ def download_anaconda_install_script(anaconda_install_script_path, quiet=False):
         copyfileobj(response, f)
 
 
-def check_hash(anaconda_install_script_path, quiet=False):
-    if not quiet:
-        print(f'checking hash of {anaconda_install_script_path}')
+def check_hash(anaconda_install_script_path):
+    print(f'checking hash of {anaconda_install_script_path}')
     with open(anaconda_install_script_path, 'rb') as f:
         if sha256(f.read()).hexdigest() != ANACONDA_HASH:
             raise RuntimeError(f'hash check failed for {ANACONDA_URL}')
 
 
+def install_anaconda(anaconda_install_script_path):
+    input(
+        'installing Anaconda3. When prompted, specify the following '
+        f'install location:\n{ANACONDA_DIR}\npress ENTER to '
+        'continue >>>'
+    )
+    subprocess.run(('bash', anaconda_install_script_path))
+
+
+def configure_anaconda():
+    subprocess.run(CONDA_PATH, 'config', '--add', 'channels', 'r')
+    subprocess.run(CONDA_PATH, 'config', '--add', 'channels', 'bioconda')
+    subprocess.run(CONDA_PATH, 'install', 'pysam')
+
+
 def parse_arguments():
     parser = ArgumentParser(description='download and install WASP')
-    parser.add_argument(
-        '--quiet',
-        action='store_true',
-        help='suppress status updates'
-    )
     parser.add_argument(
         '--tmp-dir',
         metavar='<dir/for/temp/files>',
@@ -91,14 +100,7 @@ def main():
         anaconda_install_script_path = os.path.join(
             temp_dir, 'Anaconda3-2019.03-Linux-x86_64.sh'
         )
-        download_anaconda_install_script(
-            anaconda_install_script_path,
-            quiet=args.quiet
-        )
-        check_hash(anaconda_install_script_path, quiet=args.quiet)
-        input(
-            'installing Anaconda3. When prompted, specify the following '
-            f'install location:\n{ANACONDA_DIR}\npress ENTER to '
-            'continue >>>'
-        )
-        subprocess.run(('bash', anaconda_install_script_path))
+        download_anaconda_install_script(anaconda_install_script_path)
+        check_hash(anaconda_install_script_path)
+        install_anaconda(anaconda_install_script_path)
+    configure_anaconda()
